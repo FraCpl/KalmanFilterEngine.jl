579599edbf5310e9e801893734a6476780c2b733
FraCpl <fra.capolupo@gmail.com>
2025-06-27 10:51:23 +0200

    tests

--- test/odeAllocations.jl
+++ test/odeAllocations.jl
@@ -1,17 +1,22 @@
 using KalmanFilterEngine, BenchmarkTools, ComponentArrays
 using LinearAlgebra
 
-@views function odetest(t, x, Δt, f, K1, K2, K3, K4; nSteps=1)
-    h = Δt/nSteps
+@views function odetest(t, x, Δt, f!; K1=similar(x), K2=similar(x), K3=similar(x), K4=similar(x), tmp=similar(x), nSteps=1)
+    h = Δt / nSteps
+    @inbounds for _ in 1:nSteps
+        f!(K1, t, x)
 
-    for _ in 1:nSteps
-        f(K1, t, x)
-        f(K2, t + 1/3*h, x + h*K1/3)
-        f(K3, t + 2/3*h, x  + h*(- K1/3 + K2))
-        f(K4, t + h, x + h*(K1 - K2 + K3))
+        @. tmp = x + (h/3) * K1
+        f!(K2, t + h/3, tmp)
+
+        @. tmp = x + h*(-K1/3 + K2)
+        f!(K3, t + 2h/3, tmp)
+
+        @. tmp = x + h*(K1 - K2 + K3)
+        f!(K4, t + h, tmp)
 
         t += h
-        x .+= (K1 + 3K2 + 3K3 + K4)/8
+        @. x += h*(K1 + 3K2 + 3K3 + K4)/8
     end
 end
 
@@ -31,16 +36,29 @@ function main()
     X = ComponentArray(x = x0; Φ = Φ0)
     dX = ComponentArray(x = zeros(12); Φ = zeros(12, 12))
 
-    function ff!(dX, t, X)
-        dX.x = dx
-        mul!(dX.Φ, dJ, X.Φ)
-        #dX.Φ = dJ*X.Φ
+    function ff!(dx, t, x)
+        @inbounds @simd for i in eachindex(x)
+            dx[i] = -x[i]
+        end
+    end
+    function ff(t, x)
+        dx = similar(x)
+        ff!(dx, t, x)
+        return dx
     end
 
-    K1 = similar(X); K2 = similar(X); K3 = similar(X); K4 = similar(X)
-    odetest(t0, X, Δt, ff!, K1, K2, K3, K4; nSteps=1)
+    @time ff!(dX, t, X)
+
+    K1 = similar(X); K2 = similar(X); K3 = similar(X); K4 = similar(X); K5 = similar(X)
+
+    XTRUE = KalmanFilterEngine.odeCore(t0, X, Δt, ff)
+    odetest(t0, X, Δt, ff!, K1, K2, K3, K4, K5)
+    @show norm(XTRUE - X)
+
+    @btime KalmanFilterEngine.odeCore($t0, $X, $Δt, $ff)
+    @btime odetest($t0, $X, $Δt, $ff!, $K1, $K2, $K3, $K4, $K5)
 
     #@show norm(x - X.x) + norm(P - X.Φ)
     return nothing
 end
-@btime main()
+main()
